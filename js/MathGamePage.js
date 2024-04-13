4b7a2168e161df9513d53471027ace944cb2d39d
AalexandraNicole <126488578+AalexandraNicole@users.noreply.github.com>
2024-04-13 21:39:46 +0300

    MathGame

--- js/MathGamePage.js
+++ js/MathGamePage.js
@@ -1,3 +1,78 @@
+let menuButton = document.querySelector(".openMen");
+let menuBar = document.querySelector(".menuBar");
+let menuButtons = document.querySelectorAll(".menuButtons");
+let rankBox = document.querySelector(".ranksBox");
+let doctorBox = document.querySelector(".doctorBox");
+let doctorButton = document.querySelector(".doctorButton");
+let nameYear = document.querySelector(".nameYear");
+let contact = document.querySelector(".contact");
+let about = document.querySelector(".about");
+let admin = document.querySelector(".admin");
+let infoP = document.querySelector(".infoP");
+let terms = document.querySelector(".terms");
+let privacy = document.querySelector(".privacy");
+let trigger = 1;
+
+function showMenu() {
+    if (trigger === 1) {
+        menuButton.style.backgroundImage = "url('../images/menuOpen.png')";
+        menuBar.style.visibility = "hidden";
+        trigger = 0;
+    } else {
+        menuButton.style.backgroundImage = "url('../images/menuClose.webp')";
+        menuBar.style.visibility = "visible";
+        trigger = 1;
+    }
+};
+
+function showMenuButtons() {
+    menuButtons.forEach(function (button) {
+        button.style.visibility = "visible";
+    })
+
+    nameYear.style.visibility = "visible";
+    contact.style.visibility = "visible";
+    about.style.visibility = "visible";
+    admin.style.visibility = "visible";
+    infoP.style.visibility = "visible";
+    privacy.style.visibility = "visible";
+    terms.style.visibility = "visible";
+
+    menuBar.scroll = 0;
+};
+
+function hideMenuButtons() {
+    menuButtons.forEach(function (button) {
+        button.style.visibility = "hidden";
+    })
+
+    nameYear.style.visibility = "hidden";
+    contact.style.visibility = "hidden";
+    about.style.visibility = "hidden";
+    admin.style.visibility = "hidden";
+    infoP.style.visibility = "hidden";
+    privacy.style.visibility = "hidden";
+    terms.style.visibility = "hidden";
+};
+
+function showRanks() {
+    rankBox.style.visibility = "visible";
+}
+
+function hideRanks() {
+    rankBox.style.visibility = "hidden";
+}
+
+function showDoctorBox() {
+    doctorButton.style.backgroundImage = "url('../images/meds.gif')";
+    doctorBox.style.visibility = "visible";
+}
+
+function hideDoctorBox() {
+    doctorButton.style.backgroundImage = "url('../images/med.gif')";
+    doctorBox.style.visibility = "hidden";
+}
+
 var playing = false;
 var score;
 var action;
@@ -103,7 +178,6 @@ function show(Id) {
 //guestion
 function getRandomFruitImageUrl() {
     var fruitImages = [
-        "../images/mstrawberry.jpg",
         "../images/apple.jpg",
         "../images/orange.jpg",
         "../images/mango.avif",
@@ -117,13 +191,32 @@ function getRandomFruitImageUrl() {
 function generateQA() {
     var x = 1 + Math.round(9 * Math.random());
     var y = 1 + Math.round(9 * Math.random());
-    correctAnswer = x * y;
+    correctAnswer = x + y;
 
     // Generate a random fruit image URL
     var fruitImageUrl = getRandomFruitImageUrl();
 
-    // Set question with fruit images and operator
-    document.getElementById("question").innerHTML = generateFruitImages(x) + " x " + generateFruitImages(y);
+    if( x == 0 ){
+        document.getElementById("question").innerHTML = "<img src='../images/zero.jpg' alt='Fruit Image'> " + " + " + generateFruitImages(y);
+    }else if( y == 0){
+        document.getElementById("question").innerHTML = generateFruitImages(x) + " + " + "<img src='../images/zero.jpg' alt='Fruit Image'> ";
+    } else {
+        // Set question with fruit images and operator
+        document.getElementById("question").innerHTML = generateFruitImages(x) + " + " + generateFruitImages(y);
+    }
+    // Check if either group has more than four images
+    var xImages = document.querySelectorAll("#question .x-image").length;
+    var yImages = document.querySelectorAll("#question .y-image").length;
+    if (xImages > 4) {
+        document.getElementById("question").classList.add("x-multiple-lines");
+    } else {
+        document.getElementById("question").classList.remove("x-multiple-lines");
+    }
+    if (yImages > 4) {
+        document.getElementById("question").classList.add("y-multiple-lines");
+    } else {
+        document.getElementById("question").classList.remove("y-multiple-lines");
+    }
 
     var correctPosition = 1 + Math.round(3 * Math.random());
 
