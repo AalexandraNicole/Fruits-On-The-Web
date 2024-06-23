942ea8d3e018b8058005b2a9cb63b423c58133e0
Grozea Tatiana <taneagrozea2003@gmail.com>
2024-06-23 23:30:55 +0300

    remove handle session middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,13 @@ const http = require("http");
 const routes = require("./routes");
 const matchRoute = require("./middleware/match-route");
 const verifyToken = require("./middleware/verify-token");
-const handleSession = require("./middleware/handle-session");
 const mongoDb = require("./services/mongo-db");
 
 (async () => {
-  const sessionStore = {};
   const mongoDbService = await mongoDb();
-  const services = new Map([
-    ["mongodb", mongoDbService],
-    ["sessionStore", sessionStore],
-  ]);
+  const services = new Map([["mongodb", mongoDbService]]);
 
   const server = http.createServer(async (req, res) => {
-    await handleSession(req, res, services);
     await matchRoute(req, res, routes, services);
   });
 
