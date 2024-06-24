df6fde3b05ed71cdc439dff4921ce6437961699f
Grozea Tatiana <taneagrozea2003@gmail.com>
2024-06-24 23:52:30 +0300

    all done

--- backend/src/environment.js
+++ backend/src/environment.js
@@ -1,8 +1,6 @@
 module.exports = {
-  connectionString:,
-  dbName: ,
-  gmailUser: ,
-  gmailPas: ,
+  connectionString: "",
+  dbName: "",
+  gmailUser: "",
+  gmailPas: "",
 };
- 
-//https://online.visual-paradigm.com/share.jsp?id=333434383131382d31
